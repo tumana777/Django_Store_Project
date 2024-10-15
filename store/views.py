b06e2c6ffc8532fdb6708932750779752c36fbc0
tumana777 <tumana991@gmail.com>
2024-10-15 14:49:36 +0400

    Improve frontend appearance

--- store/views.py
+++ store/views.py
@@ -4,6 +4,7 @@ from django.db.models import F, Max, Avg, Min, Sum
 
 
 def category_list(request):
+
     categories = Category.objects.filter(parent=None)
 
     for category in categories:
@@ -14,6 +15,7 @@ def category_list(request):
     return render(request, 'categories.html', {"categories": categories})
 
 def product_list(request, category_id):
+
     main_category = Category.objects.get(id=category_id)
 
     all_categories = main_category.get_descendants(include_self=True)
@@ -21,9 +23,6 @@ def product_list(request, category_id):
     products = (Product.objects.filter(category__in=all_categories)
                 .prefetch_related('category').distinct()
                 .annotate(total=F('quantity') * F('price')))
-
-    for product in products:
-        print(product.total)
 
     stats = products.aggregate(max_price=Max('price'),
                                min_price=Min('price'),
@@ -39,5 +38,7 @@ def product_list(request, category_id):
     return render(request, 'products.html', context)
 
 def product_detail(request, product_id):
-    product = Product.objects.get(id=product_id)
+
+    product = Product.objects.prefetch_related('category').get(id=product_id)
+
     return render(request, 'product_detail.html', {"product": product})
